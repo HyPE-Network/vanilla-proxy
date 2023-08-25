8c530aea3864a37b5035d88eb2642baef3669700
Ermolay <es.ermolaev2005@gmail.com>
2023-08-25 21:21:12 +0300

    Simplifying startup

--- main.go
+++ main.go
@@ -12,33 +12,13 @@ func main() {
 	log.Logger = log.New()
 	log.Logger.Debugln("Logger has been started")
 
-	log.Logger.Debugln("Loading proxy data..")
-	if err := load(); err != nil {
-		log.Logger.Errorln("Error while loading proxy: ", err)
-		panic(err)
-	}
-
-	log.Logger.Debugln("Proxy is starting...")
-	if err := start(); err != nil {
-		log.Logger.Errorln("Error while starting server: ", err)
-		panic(err)
-	}
-}
-
-func load() error {
-	var err error = nil
-
 	config := utils.ReadConfig()
 
 	proxy.ProxyInstance = proxy.New(config, manager.NewPlayerManager())
 
-	return err
-}
-
-func start() error {
-	var err error = nil
-
-	err = proxy.ProxyInstance.Start(handlers.New())
-
-	return err
+	err := proxy.ProxyInstance.Start(handlers.New())
+	if err != nil {
+		log.Logger.Errorln("Error while starting server: ", err)
+		panic(err)
+	}
 }
