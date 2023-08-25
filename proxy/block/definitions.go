f9da1783d21cf81798f6f953221c74837aa052d6
Ermolay <es.ermolaev2005@gmail.com>
2023-08-25 21:32:07 +0300

    Delete unused blocks

--- proxy/block/definitions.go
+++ proxy/block/definitions.go
@@ -1,14 +1,5 @@
 package block
 
 const (
-	GoldBlock           = "minecraft:gold_block"
-	DeepslateDiamondOre = "minecraft:deepslate_diamond_ore"
-	DiamondOre          = "minecraft:diamond_ore"
-	IronBlock           = "minecraft:iron_block"
-	NetheriteBlock      = "minecraft:netherite_block"
-	Chest               = "minecraft:chest"
+	Chest = "minecraft:chest"
 )
-
-func IsDiamondOre(rid int32) bool {
-	return GetBlockName(rid) == DiamondOre || GetBlockName(rid) == DeepslateDiamondOre
-}
