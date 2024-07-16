8789f245f40677e33349a8a5e81068ae17094993
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-07-16 17:17:32 -0400

    remove logging

--- custom_handlers/signEditHandler.go
+++ custom_handlers/signEditHandler.go
@@ -1,7 +1,6 @@
 package custom_handlers
 
 import (
-	"github.com/HyPE-Network/vanilla-proxy/log"
 	"github.com/HyPE-Network/vanilla-proxy/proxy/player/human"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
@@ -12,7 +11,7 @@ type SignEditHandler struct {
 func (SignEditHandler) Handle(pk packet.Packet, player human.Human) (bool, packet.Packet, error) {
 	dataPacket := pk.(*packet.BlockActorData)
 
-	log.Logger.Debugln("Update Block: ", dataPacket.Position, dataPacket.NBTData)
+	//log.Logger.Debugln("Update Block: ", dataPacket.Position, dataPacket.NBTData)
 
 	return true, dataPacket, nil
 }
--- custom_handlers/textHandler.go
+++ custom_handlers/textHandler.go
@@ -1,8 +1,6 @@
 package custom_handlers
 
 import (
-	"log"
-
 	"github.com/HyPE-Network/vanilla-proxy/proxy/player/human"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
@@ -13,7 +11,7 @@ type TextHandler struct {
 func (TextHandler) Handle(pk packet.Packet, player human.Human) (bool, packet.Packet, error) {
 	dataPacket := pk.(*packet.Text)
 
-	log.Println("TextHandler: ", dataPacket.Message)
+	//log.Println("TextHandler: ", dataPacket.Message)
 
 	return true, dataPacket, nil
 }
