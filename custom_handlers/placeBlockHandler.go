976d60e0fc102b06bb8878fcf733abf6909ebad7
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-07-20 11:56:43 -0400

    Update placeBlockHandler.go

--- custom_handlers/placeBlockHandler.go
+++ custom_handlers/placeBlockHandler.go
@@ -1,7 +1,6 @@
 package custom_handlers
 
 import (
-	"github.com/HyPE-Network/vanilla-proxy/proxy/block/cube"
 	"github.com/HyPE-Network/vanilla-proxy/proxy/player/human"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
@@ -18,10 +17,8 @@ func (PlaceBlockHandler) Handle(pk packet.Packet, player human.Human) (bool, pac
 		if td.ActionType == protocol.UseItemActionClickBlock {
 			if td.HeldItem.Stack.BlockRuntimeID != 0 {
 				if td.HeldItem.Stack.ItemType.NetworkID == 49 && player.InNether() { // obsidian block in nether
-					pos := cube.Side(td.BlockPosition, td.BlockFace)
-
 					player.SendMessage("§cYou can't place obsidian in this world!")
-					player.SendAirUpdate(protocol.BlockPos{pos.X(), pos.Y(), pos.Z()})
+					player.SendAirUpdate(td.BlockPosition)
 					return false, pk, nil
 				}
 			}
