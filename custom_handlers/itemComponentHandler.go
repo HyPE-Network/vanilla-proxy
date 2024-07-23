1ef244600efc1de1711f318221310a6b90d63511
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2024-07-23 10:41:47 -0400

    Create itemComponentHandler.go

---
+++ custom_handlers/itemComponentHandler.go
@@ -0,0 +1,17 @@
+package custom_handlers
+
+import (
+	"github.com/HyPE-Network/vanilla-proxy/proxy/player/human"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
+)
+
+type ItemComponentHandler struct {
+}
+
+func (ItemComponentHandler) Handle(pk packet.Packet, player human.Human) (bool, packet.Packet, error) {
+	dataPacket := pk.(*packet.ItemComponent)
+
+	player.SetItemComponentEntries(dataPacket.Items)
+
+	return true, dataPacket, nil
+}
